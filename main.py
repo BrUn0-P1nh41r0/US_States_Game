675e01b467b39cbbad5e6f57a8485bb1c58bc4ba
Bruno <141152582+BrUn0-P1nh41r0@users.noreply.github.com>
2025-01-14 14:06:46 +0000

    end of day 25 / US Game

--- main.py
+++ main.py
@@ -20,14 +20,19 @@ states = pandas.read_csv("50_states.csv")
 all_states = states.state.to_list()
 
 guessed_states = []
-game_is_on = True
 
-while game_is_on:
+while len(guessed_states) < 50:
 
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?")
 
     if answer_state == "exit":
-        game_is_on = False
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_missing.csv")
+        break
 
     if answer_state in all_states and answer_state not in guessed_states:
         guessed_states.append(answer_state)
@@ -37,8 +42,3 @@ while game_is_on:
         text.goto(x, y)
         text.write(answer_state, align=ALIGNMENT, font= FONT)
 
-    if len(guessed_states) >= 50:
-        game_is_on = False
-
-
-screen.exitonclick()
